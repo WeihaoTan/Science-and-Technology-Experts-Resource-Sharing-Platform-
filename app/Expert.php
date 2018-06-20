905ee8ef88ce4b990b98774104d0c6e1e489b265
slzf <280943872@qq.com>
2018-06-20 22:41:37 +0800

    删除了modInfo接口的教育经历字段

--- app/Expert.php
+++ app/Expert.php
@@ -19,10 +19,10 @@ class  Expert extends Model
         return $this->where('expert_id', $expert_id)->get();
     }
 
-    public function modInfo($expert_id, $institution, $title, $educational_experience,
+    public function modInfo($expert_id, $institution, $title,
                             $occupational_experience, $award_winning_experience, $field, $expert_name){
         return $this->where('expert_id', $expert_id)
-            ->update(['institution' => $institution, 'title' => $title, 'educational_experience' => $educational_experience,
+            ->update(['institution' => $institution, 'title' => $title,
                 'occupational_experience' => $occupational_experience, 'award_winning_experience' => $award_winning_experience,
                 'field' => $field, 'expert_name' => $expert_name]);
     }
