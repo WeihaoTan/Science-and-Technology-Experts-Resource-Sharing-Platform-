414e51fe82ac02e8a5e6c5524e39512bb484e920
iamfitz <34505007+iamfitz@users.noreply.github.com>
2018-06-20 23:27:30 +0800

    Create paper_key.php

---
+++ app/Http/Models/paper_key.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: iamfitz
+ * Date: 2018/6/20
+ * Time: 1:17
+ */
+
+namespace App\Http\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class paper_key extends Model
+{
+    protected $table = 'paper_key';
+}
