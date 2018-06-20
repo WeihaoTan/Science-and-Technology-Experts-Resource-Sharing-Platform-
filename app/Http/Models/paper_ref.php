96896a66265fc28436686d6cae3174804140a3fa
iamfitz <34505007+iamfitz@users.noreply.github.com>
2018-06-20 23:28:22 +0800

    Create paper_ref.php

---
+++ app/Http/Models/paper_ref.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: iamfitz
+ * Date: 2018/6/20
+ * Time: 23:23
+ */
+
+namespace App\Http\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class paper_ref extends Model
+{
+    protected $table = 'paper_ref';
+}
