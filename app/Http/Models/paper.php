0fa4d44a26e3f3db7c6ee0bbba7d6f0a29a7805a
iamfitz <34505007+iamfitz@users.noreply.github.com>
2018-06-21 22:37:31 +0800

    Update paper.php

--- app/Http/Models/paper.php
+++ app/Http/Models/paper.php
@@ -19,7 +19,7 @@ class paper extends Model
     public $timestamps=false;
      ////论文列表
     public function paperList(array $request){
-        return $this::whereRaw('MATCH(paper_name) AGAINST(?)',$request['paper_name'])
+        return $this::whereRaw('MATCH(paper_name) AGAINST(? IN NATURAL LANGUAGE MODE)',$request['paper_name'])
                     ->join('expert', 'expert.expert_id', '=', 'paper.first_author_id')
                     ->select('paper_id','access','paper_name','expert_name',
                         'first_author_id','publish_time','abstract','url','type')
@@ -65,7 +65,7 @@ class paper extends Model
         $paper_key = new paper_key();
         $paper_id = $paper_key->whereIn('key',[$request['keyword1'],$request['keyword2'],$request['keyword3']])->select('paper_id')->distinct()->get();
         return $this::whereIn('paper_id',$paper_id)
-            ->whereRaw('MATCH(paper_name) AGAINST(?)',$request['paper_name'])
+            ->whereRaw('MATCH(paper_name) AGAINST(? IN NATURAL LANGUAGE MODE)',$request['paper_name'])
             ->whereBetween('publish_time',[$request['start_time'],$request['end_time']])
             ->join('expert', 'expert.expert_id', '=', 'paper.first_author_id')
             ->select('paper_id','access','paper_name','expert_name',
